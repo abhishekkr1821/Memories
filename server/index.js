4ba81587040e9b6c60141e613c539b1588fa757a
Abhishek Kumar <abhishekkr.sah2008@gmail.com>
2024-07-23 21:49:28 +0530

    first

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL =
+  "mongodb+srv://abhishekkrsah:abhishek@cluster0.zzpu7rq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: ${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
