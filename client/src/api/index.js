92d7f76a30eb0cf82320e23d918fc1f1ae9fe8a4
Abhishek Kumar <124042306+abhishekkr1821@users.noreply.github.com>
2024-07-24 02:11:39 +0530

    index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://memories-backend-95p7.onrender.com';
+const url = "https://memories-backend-95p7.onrender.com";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
